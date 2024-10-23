03095f5c94a8888539d2699d0d46d596d4d85dfa
a23842 <372624240@qq.com>
2024-10-23 17:54:30 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -90,11 +90,11 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["hs.looknet.asia"];
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
-    _ => "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=",
+    _ => "vuPlTsXLe+qdaaDLVnlQXSe8lxhj7ob44Aygb75MAO0=",
 };
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
